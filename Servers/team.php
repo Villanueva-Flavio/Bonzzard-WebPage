32b27b4518a613fbc49e77e1d5c3adb1da806037
Flavio Villanueva <fvillanueva@fi.uba.ar>
2024-05-07 19:02:06 -0300

    [FIX] Cant use fullpagejs anymore, thanks AlvaroTrigo :)

--- Servers/team.php
+++ Servers/team.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../Assets/Styles/team.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.1.2/fullpage.min.js"></script>   
+    <?php include "/Assets/Includes/script.php"; ?>
     <title>Bonzzard > Team</title>
 </head>
 <body>
@@ -17,17 +17,19 @@
     </div>
 
     <script>
-        new fullpage('#fullpage', {
-            licenseKey: null,
-            scrollBar: false,
-            navigation: true,
-            navigationTooltips: ['Team', 'About', 'Contact'],
-            showActiveTooltip: true,
-            scrollingSpeed: 1000,
-            slidesNavigation: true,
-            slidesNavPosition: 'bottom',
-            trigger: 'slideArrow',
+        // Función para desplazarse suavemente a una sección al hacer clic en un enlace
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                const targetId = this.getAttribute('href').substring(1); // Obtener el ID de la sección a la que queremos desplazarnos
+
+                document.getElementById(targetId).scrollIntoView({
+                    behavior: 'smooth' // Desplazamiento suave
+                });
+            });
         });
+
     </script>
 
 </body>
